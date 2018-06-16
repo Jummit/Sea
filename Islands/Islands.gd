8856e53a9965ebb9e6c9f808882c2c01069fdff5
Jummit <jummit@web.de>
2018-06-16 08:18:02 +0200

    let sea not generate water below islands

--- Islands/Islands.gd
+++ Islands/Islands.gd
@@ -1,8 +1,9 @@
 extends TileMap
 
-func _ready():
+func _on_Sea_generated(sea):
 	for i in range(1, 100):
 		var pos = Vector2(rand_range(-100, 100), rand_range(-100, 100))
 		for x in range(-3, 3):
 			for y in range(-3, 3):
+				sea.set_cell(pos.x+x, pos.y+y, -1)
 				set_cell(pos.x+x, pos.y+y, 0)
--- Sea/Sea.gd
+++ Sea/Sea.gd
@@ -1,6 +1,9 @@
 extends TileMap
 
+signal generated
+
 func _ready():
 	for x in range(-100, 100):
 		for y in range(-100, 100):
 			set_cell(x, y, 0)
+	emit_signal("generated", self)
