e972311ce28cd601a74c262ceb88226cdb8b5788
Jummit <jummit@web.de>
2018-06-23 15:11:55 +0200

    cant spam inventory button while animation is playing

--- UI/Inventory/InventoryButton.gd
+++ UI/Inventory/InventoryButton.gd
@@ -10,7 +10,9 @@ func _process(delta):
 		ui.close_inventory()
 
 func _pressed():
-	if ui.inventory_open:
-		ui.close_inventory()
-	else:
-		ui.open_inventory()
+	var animation = ui.get_node("Inventory/AnimationPlayer")
+	if animation.get_current_animation_length() == animation.get_current_animation_pos():
+		if ui.inventory_open:
+			ui.close_inventory()
+		else:
+			ui.open_inventory()
