03a27d733193197c319dded057a3b5d0b180f91c
Jummit <jummit@web.de>
2018-06-23 15:01:08 +0200

    added animation to inventory panel

--- UI/UI.gd
+++ UI/UI.gd
@@ -2,13 +2,20 @@ extends CanvasLayer
 
 onready var stats = get_node("Stats")
 onready var inventory = get_node("Inventory")
-
+onready var inventoryAnimation = inventory.get_node("AnimationPlayer")
 var inventory_open = false
 
 func open_inventory():
-	inventory_open = true
 	inventory.show()
+	inventoryAnimation.set_current_animation("OpenAnimation")
+	inventoryAnimation.play()
+	inventory_open = true
 
 func close_inventory():
+	inventoryAnimation.set_current_animation("CloseAnimation")
+	inventoryAnimation.play()
 	inventory_open = false
-	inventory.hide()
+
+func _on_AnimationPlayer_finished():
+	if inventory_open == false:
+		inventory.hide()
