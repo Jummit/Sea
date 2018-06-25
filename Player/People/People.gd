f02a42e8ddd4c23c835052509302766ff836397b
Jummit <jummit@web.de>
2018-06-25 19:51:19 +0200

    player walk animation works

--- Player/People/People.gd
+++ Player/People/People.gd
@@ -1,7 +1,18 @@
 extends KinematicBody2D
 
 onready var villages = get_node("../../Villages")
+onready var sprite = get_node("Sprite")
 var speed = 100
 
+func _ready():
+	set_process(true)
+
 func makeMove(move):
-	move_and_slide(move*speed)
+	if move == Vector2():
+		sprite.set_animation("Idle")
+		sprite.stop()
+		sprite.set_frame(0)
+	else:
+		sprite.set_animation("Walking")
+		sprite.play()
+		move_and_slide(move*speed)
