72e51b2e91836664be5fd22dea5a7c9a1fde7345
Jummit <jummit@web.de>
2018-06-29 12:24:53 +0200

    added rain

---
+++ Rain/Rain.gd
@@ -0,0 +1,20 @@
+extends CanvasLayer
+
+onready var player = get_node("../Player")
+onready var particles = get_node("Particles")
+onready var timer = get_node("Timer")
+onready var animationplayer = get_node("AnimationPlayer")
+var raining = true
+
+func _ready():
+	set_process(true)
+
+func _process(delta):
+	var screensize = get_viewport().get_rect().size
+	particles.set_emission_half_extents(screensize)
+
+func toggle_rain():
+	print("Rain toggling!")
+	raining = not raining
+	particles.set_emitting(raining)
+	timer.set_wait_time(rand_range(2, 7)*60)
