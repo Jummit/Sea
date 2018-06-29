78618c53a26ad01b23bc32978043be6c8ee11b5b
Jummit <jummit@web.de>
2018-06-29 16:36:34 +0200

    items are clickable again

--- Inventory/Items/Item.gd
+++ Inventory/Items/Item.gd
@@ -20,9 +20,17 @@ func acquire():
 	texture.set_material(CanvasItemMaterial.new())
 
 func _input_event( ev ):
+	print("test")
 	if ev.type == InputEvent.MOUSE_BUTTON:
 		get_node("../../../../../../").set_info(get_name(), get_node("Description").get_text(), texture.get_texture())
 
 func _process(delta):
 	if action.has_method("process"):
 		action.process(acquired)
+
+func _on_Area2D_input_event( viewport, event, shape_idx ):
+	print(event.type)
+
+
+func _on_Button_pressed():
+	get_node("../../../../../../").set_info(get_name(), get_node("Description").get_text(), texture.get_texture())
